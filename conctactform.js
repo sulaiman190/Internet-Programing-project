be5cffc5ce04a33177f48947e48db7ef4ea49a46
Sulaiman Jalloh <sjalloh105@gmail.com>
2018-11-16 03:18:32 -0500

    Add files via upload

---
+++ conctactform.js
@@ -0,0 +1,76 @@
+function conctactformValidation()
+{
+var name1 = document.registration.fname;
+var name2 = document.registration.lname;
+var mnum = document.registration.Mnumber;
+var mailat = document.registration.email;
+
+ if(fname_validation(name1))
+{
+if(lname_validation(name2))
+{
+if(alphanumeric(addy))
+  { 
+if(nums(mnum))
+{
+if(ValidateEmail(mailat))
+{
+if(validsex(umsex,ufsex)) {
+        }
+     } 
+   }
+  } 
+ }
+}
+return false;
+}
+  function fname_validation(name1)
+{
+  var name1_len = name1.value.length;
+       if (name1_len == 0 )
+   {
+alert("Please enter your first name");
+   name1.focus();
+   return false;
+}
+    return true;
+}
+function lname_validation(name2)
+{
+   var name2_len = name2.value.length;
+   if (name2_len == 0 )
+{
+alert("Please enter your last name");
+   name2.focus();
+   return false;
+}
+   return true;
+}
+function nums(mnum)
+{ 
+var digits = /^[0-9]+$/;
+     if(mnum.value.match(digits))
+{
+return true;
+}
+else
+{
+alert('Please enter your Mobile number');
+mnum.focus();
+    return false;
+   }
+}
+function ValidateEmail(mailat)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+   if(mailat.value.match(mailformat))
+   {
+     return true;
+}
+ else
+ {
+alert("Please enter a valid email address");
+return false;
+      }
+
+}
